0121475ba32ebd5e324c9c19a1751c62bc49d0d1
Pierre Boudes <boudes@univ-paris13.fr>
2013-06-27 15:09:06 +0200

    ajout d'un readme et d'une licence explicite (gpl v3)

---
+++ README.md
@@ -0,0 +1,5 @@
+
+minoterie
+====
+
+companion application for pain. Minoterie takes two snapshots per year of the teaching service, for declarative purpose.
--- sources/remplissage_minoterie.sql
+++ sources/remplissage_minoterie.sql
@@ -1,19 +1,19 @@
 -- -*- coding: utf-8 -*-
 
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (1, 'chimie', 'http://servens-galilee.univ-paris13.fr/chimie/pain/');
+VALUES (1, 'chimie', '/chimie/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (2, 'informatique', 'http://servens-galilee.univ-paris13.fr/informatique/pain/');
+VALUES (2, 'informatique', '/informatique/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (3, 'mathématiques', 'http://servens-galilee.univ-paris13.fr/mathematiques/pain/');
+VALUES (3, 'mathématiques', '/mathematiques/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (4, 'physique', 'http://servens-galilee.univ-paris13.fr/physique/pain/');
+VALUES (4, 'physique', '/physique/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (5, 'lv', 'http://servens-galilee.univ-paris13.fr/lv/pain/');
+VALUES (5, 'lv', '/lv/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (6, 'com', 'http://servens-galilee.univ-paris13.fr/com/pain/');
+VALUES (6, 'com', '/com/pain/');
 INSERT INTO `minoterie_departement` (`id_departement`, `nom_departement`, `url_pain`)
-VALUES (7, 'commun', 'http://servens-galilee.univ-paris13.fr/commun/pain/');
+VALUES (7, 'commun', '/commun/pain/');
 INSERT INTO `minoterie_utilisateur` (`login`,`prenom`, `nom`, `id_departement`, `su`) VALUES ('boudes', 'Pierre', 'Boudes', 2, 1);
 INSERT INTO `minoterie_utilisateur` (`login`,`prenom`, `nom`, `id_departement`, `su`) VALUES ('boudes', 'Pierre', 'Boudes', 5, 1);
 INSERT INTO `minoterie_utilisateur` (`login`,`prenom`, `nom`, `id_departement`, `su`) VALUES ('boudes', 'Pierre', 'Boudes', 7, 1);
